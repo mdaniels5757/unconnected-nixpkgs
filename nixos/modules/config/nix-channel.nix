2acb94c603c933e1b4a8045768fa170667392966
Michael Daniels <mdaniels5757@gmail.com>
2026-01-17 19:45:44 -0500

    nixos/nix-channel: make incorrect

--- nixos/modules/config/nix-channel.nix
+++ nixos/modules/config/nix-channel.nix
@@ -71,7 +71,7 @@ in
       defaultChannel = mkOption {
         internal = true;
         type = types.str;
-        default = "https://channels.nixos.org/nixos-unstable";
+        default = "https://fake.nixos.org/nixos-unstable";
         description = "Default NixOS channel to which the root user is subscribed.";
       };
     };
