be54ca707b686b4a1ac9791f3f8028d47de94b07
Michael Daniels <mdaniels5757@gmail.com>
2026-01-18 20:26:09 -0500

    ci/check-target-branch: continue init-ing

--- ci/github-script/check-target-branch.js
+++ ci/github-script/check-target-branch.js
@@ -6,7 +6,7 @@
 
 const { classify, split } = require('../supportedBranches.js')
 const { readFile } = require('node:fs/promises')
-const { postReview, dismissReviews } = require('./reviews.js')
+const { postReview } = require('./reviews.js')
 
 /**
  * @param {{
@@ -45,33 +45,87 @@ async function checkTargetBranch({ github, context, core, dry }) {
     )
     return
   }
+  // Don't run on PRs against staging branches, wip branches, haskell-updates, etc.
+  if (!baseClassification.type.includes('primary')) {
+    core.info(
+      `Skipping checkTargetBranch: PR is against a non-primary base branch (${base})`,
+    )
+    return
+  }
 
   const maxRebuildCount = Math.max(
     ...Object.values(changed.rebuildCountByKernel),
   )
-  const rebuildsAllTests = changed.labels['10.rebuild-nixos-tests']
+  const rebuildsAllTests = changed.labels['10.rebuild-nixos-tests'] ?? false
+  core.info(`checkTargetBranch: PR causes ${maxRebuildCount} rebuilds and ${rebuildsAllTests ? 'does' : 'does not'} rebuild all NixOS tests.`)
 
-  // TODO: this only requests changes when rebuild count is above 1000, because
-  // this is the absolute maximum for master per CONTRIBUTING.md.
-  // Should we add a comment when 500 <= maxRebuildCount < 1000? (This would require changes to reviews.js, I believe.)
-  if (maxRebuildCount >= 1000 && baseClassification.type.includes('primary')) {
+  if (maxRebuildCount >= 1000) {
     const desiredBranch =
       base === 'master' ? 'staging' : `staging-${split(base).version}`
-    const body = `The PR's base branch is set to \`${base}\`, but this PR causes more than 1000 rebuilds. Please [change the base branch](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/changing-the-base-branch-of-a-pull-request) to [the right base branch for your changes](https://github.com/NixOS/nixpkgs/blob/master/CONTRIBUTING.md#branch-conventions) (probably \`${desiredBranch}\`).`
+    const body = [
+      `The PR's base branch is set to \`${base}\`, but this PR causes ${maxRebuildCount} rebuilds.`,
+      'It is therefore considered a mass rebuild.',
+      `Please [change the base branch](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/changing-the-base-branch-of-a-pull-request) to [the right base branch for your changes](https://github.com/NixOS/nixpkgs/blob/master/CONTRIBUTING.md#branch-conventions) (probably \`${desiredBranch}\`).`,
+    ].join('\n')
 
-    await postReview({ github, context, core, dry, body })
+    await postReview({
+      github,
+      context,
+      core,
+      dry,
+      body,
+      event: 'REQUEST_CHANGES',
+    })
 
     throw new Error('This PR is against the wrong branch.')
-  } else if (rebuildsAllTests && baseClassification.type.includes('primary')) {
-    const desiredBranch =
-      base === 'master' ? 'staging-nixos' : `staging-${split(base).version}`
-    const body = `The PR's base branch is set to \`${base}\`, but this PR rebuilds all NixOS tests. Please [change the base branch](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/changing-the-base-branch-of-a-pull-request) to [the right base branch for your changes](https://github.com/NixOS/nixpkgs/blob/master/CONTRIBUTING.md#branch-conventions) (probably \`${desiredBranch}\`).`
-    await postReview({ github, context, core, dry, body })
+  } else if (rebuildsAllTests) {
+    let branchText
+    if (base === 'master' && maxRebuildCount >= 500) {
+      branchText = '(probably either `staging-nixos` or `staging`)'
+    } else if (base === 'master') {
+      branchText = '(probably `staging-nixos`)'
+    } else {
+      branchText = `(probably \`staging-${split(base).version}\`)`
+    }
+    const body = [
+      `The PR's base branch is set to \`${base}\`, but this PR rebuilds all NixOS tests.`,
+      base === 'master' && maxRebuildCount >= 500
+        ? `Since this PR also causes ${maxRebuildCount} rebuilds, it may also be considered a mass rebuild.`
+        : '',
+      `Please [change the base branch](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/changing-the-base-branch-of-a-pull-request) to [the right base branch for your changes](https://github.com/NixOS/nixpkgs/blob/master/CONTRIBUTING.md#branch-conventions) ${branchText}.`,
+    ].join('\n')
+
+    await postReview({
+      github,
+      context,
+      core,
+      dry,
+      body,
+      event: 'REQUEST_CHANGES',
+    })
 
     throw new Error('This PR is against the wrong branch.')
+  } else if (maxRebuildCount >= 500) {
+    const stagingBranch =
+      base === 'master' ? 'staging' : `staging-${split(base).version}`
+    const body = [
+      `The PR's base branch is set to \`${base}\`, and this PR causes ${maxRebuildCount} rebuilds.`,
+      `Please consider whether this PR causes a mass rebuild according to [our conventions](https://github.com/NixOS/nixpkgs/blob/master/CONTRIBUTING.md#branch-conventions).`,
+      `If it does cause a mass rebuild, please [change the base branch](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/changing-the-base-branch-of-a-pull-request) to [the right base branch for your changes](https://github.com/NixOS/nixpkgs/blob/master/CONTRIBUTING.md#branch-conventions) (probably ${stagingBranch}).`,
+      `If it does not cause a mass rebuild, this message can be ignored.`
+    ].join('\n')
+
+    await postReview({
+      github,
+      context,
+      core,
+      dry,
+      body,
+      event: 'COMMENT',
+    })
   } else {
     // Any existing reviews were dismissed by commits.js
-    core.info('checkTargetBranch: this PR was against an appropriate branch.')
+    core.info('checkTargetBranch: this PR is against an appropriate branch.')
   }
 }
 
